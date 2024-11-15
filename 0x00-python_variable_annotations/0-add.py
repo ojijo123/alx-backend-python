be8409bd3d023fe9252fc179a03e8f214b40eaf1
ojijo123 <oumaerick173@gmail.com>
2024-11-15 07:43:24 +0000

    add floats x,y

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-def add(a: float, b: float) -> float:
-    """Return the sum of two floats."""
-    return a + b
+"""
+    Module with typing Python add
+"""
+
+
+def add(x: float, y: float) -> float:
+    """
+        Args:
+            x: The first parameter
+            y: The second parameter
+
+        Return:
+            Sum of two floats
+
+    """
+
+    return (x + y)
